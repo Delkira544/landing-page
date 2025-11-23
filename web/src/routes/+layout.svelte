f5ace7180ce45c92cd4aa0719054449124f5a8af
Christian Verdugo <crischandel@gmail.com>
2025-11-23 11:49:42 -0300

    fix: twitter meta datos y whatssapp

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
   import '../app.css';
+  export const prerender = true;
 </script>
 
 <svelte:head>
@@ -18,15 +19,15 @@
   
   <!-- Twitter Cards para compartir en Twitter -->
   <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:domain" content="temutrack.vercel.app" />
+  <meta name="twitter:url" content="https://temutrack.vercel.app" />
   <meta name="twitter:title" content="Plataforma de Reportes Ciudadanos - Mejora tu Comunidad" />
   <meta name="twitter:description" content="Reporta problemas urbanos de manera fácil y gratuita. Conecta con tu municipalidad y mejora tu comunidad con nuestra plataforma innovadora." />
-  <meta name="twitter:image" content="https://temutrack.vercel.app/page-screen.png" /> <!-- Reemplaza con la URL de la imagen -->
-  
+  <meta name="twitter:image" content="https://temutrack.vercel.app/page-screen.png" />
+
   <!-- Otros metadatos opcionales -->
   <meta name="author" content="Temutrack" />
   <meta name="keywords" content="reportes ciudadanos, municipalidad, problemas urbanos, comunidad" />
 </svelte:head>
 
-<main>
-  <slot />
-</main>
+<slot />
